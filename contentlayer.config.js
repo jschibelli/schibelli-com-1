ed66e3b288b03bf1dc99ce102c21c550331703b4
John Schibelli <jschibelli@gmail.com>
2024-06-14 23:26:35 -0400

    chore: Update file path pattern for blog posts

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -18,7 +18,7 @@ const computedFields = {
 
 export const Blog = defineDocumentType(() => ({
   name: "Blog",
-  filePathPattern: "./posts/**/*.mdx",
+  filePathPattern: "./blog/**/*.mdx",
   contentType: "mdx",
 
   fields: {
