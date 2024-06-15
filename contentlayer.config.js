8b6dde54e7331523e126941aca27e06c257a998d
John Schibelli <jschibelli@gmail.com>
2024-06-15 00:57:13 -0400

    chore: Remove unused code related to blog layout and view components

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -16,35 +16,7 @@ const computedFields = {
 	},
 };
 
-export const Blog = defineDocumentType(() => ({
-  name: "Blog",
-  filePathPattern: "./blog/**/*.mdx",
-  contentType: "mdx",
 
-  fields: {
-    published: {
-      type: "boolean",
-    },
-    title: {
-      type: "string",
-      required: true,
-    },
-    description: {
-      type: "string",
-      required: true,
-    },
-    date: {
-      type: "date",
-    },
-    url: {
-      type: "string",
-    },
-    repository: {
-      type: "string",
-    },
-  },
-  computedFields,
-}));
 export const Project = defineDocumentType(() => ({
 	name: "Project",
 	filePathPattern: "./projects/**/*.mdx",
@@ -93,7 +65,7 @@ export const Page = defineDocumentType(() => ({
 
 export default makeSource({
 	contentDirPath: "./content",
-	documentTypes: [Page, Project, Blog],
+	documentTypes: [Page, Project],
 	mdx: {
 		remarkPlugins: [remarkGfm],
 		rehypePlugins: [
